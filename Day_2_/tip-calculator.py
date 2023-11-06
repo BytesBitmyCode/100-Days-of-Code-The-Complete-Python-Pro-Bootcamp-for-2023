7c167d2bac1e880217d75759889682db8161cc28
BytesBitMyCode <BytesBitMyCode@gmail.com>
2023-11-06 11:55:36 -0500

    This Project calculate the total amount each person should pay based on the tips.

--- Day_2_/tip-calculator.py
+++ Day_2_/tip-calculator.py
@@ -6,3 +6,17 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 
+bill = float(input("What is the total of the bill?\n"))
+
+people = int(input("How many people attended?\n"))
+
+tip = float(input("How much was the tip percentage?\n"))
+
+# Calculate the total bill including the tip
+bill_total = ((tip / 100) * bill) + bill
+
+# Calculate how much each person should pay
+bill_per_person = bill_total / people
+
+# Use an f-string to construct the message and print it
+print(f"Each person should pay: ${bill_per_person}")
